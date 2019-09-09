b2cfe1a8fd537795042d511ff3e2e06c85a64134
wicked777 <wicky.akloe@gmail.com>
2019-09-09 14:19:16 +0200

    Add route for deletion of recipe

--- app.py
+++ app.py
@@ -124,6 +124,12 @@ def update_recipe(recipe_id):
     return redirect(url_for("index"))
 
 
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipe.remove({"_id": ObjectId(recipe_id)})
+    return redirect(url_for("my_recipes"))
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
