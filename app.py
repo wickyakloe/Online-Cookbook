800e29090c4d6fbc6c1d92c42f16c59bf952fe79
wicked777 <wicky.akloe@gmail.com>
2019-09-15 13:22:13 +0200

    Remove login/signup modal and wire button to login template

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def load_user(username):
     return User(user['_id'])
 
 
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for("my_recipes"))
